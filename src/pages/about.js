19ce1f8b9dd126af86d64e095059ac749aa69fba
Sofie Ohlsson <anna.sofie.ohlsson@gmail.com>
2019-08-30 19:01:23 +0200

    lord of the rings quote

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,44 +25,65 @@ export default () => (
   <Layout>
     <div>
       <img src={chibi} />
-   
+
       <Header headerText="About me" />
 
-     
+
       <p>Hi, my name is Sofie! I'm currently studying to become a Frontend Developer. When I'm not trying to figure out some code I spend my time with something creative. I love creating things, whether it's photography, drawing, clay or anything else you can think of. </p>
-      <p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.
-      </p>
+      <p>
+        Roads go ever ever on over rock and under tree.
+        By caves where never sun has shone.
+        By streams that never find the sea.
+        Over snow by winter sown and through the merry flowers of June.
+        Over grass and over stone and under mountains in the moon.
 
-      </div>
-      
-     
-      <h3>Here are some websites I've done in the past. Please click on the picture if you are interested</h3>
-      
-      <Site
-        projectname="Learn for kids"
-        thumbnail={senti}
-        info="A site for small children to learn both how to use a computer and things like numbers while playing games!"
-        sitelink="https://sofieohlsson.github.io/learnforkids/"
-      />
+        Roads go ever ever on under cloud and under star.
+        Yet feet that wandering have gone turn at last to home afar.
+        Eyes that fire and sword have seen and horror in the halls of stone.
+        Look at last on meadows green and trees and hills they long have known.
 
-      <Site
-        projectname="Mario game"
-        thumbnail={mariogame}
-        info="I also made a mashup game of the classic pokémon fighting system with nintendo characters. ((**sound on**))"
-        sitelink="https://compassionate-lovelace-d33b19.netlify.com/"
-      />
+        The Road goes ever on and on down from the door where it began.
+        Now far ahead the Road has gone and I must follow, if I can.
+        Pursuing it with weary feet until it joins some larger way where many paths and errands meet.
+        And whither then? I cannot say.
 
+        The Road goes ever on and on out from the door where it began.
+        Now far ahead the Road has gone.
+        Let others follow, if they can!
+        Let them a journey new begin.
+        But I at last with weary feet will turn towards the lighted inn.
+        My evening-rest and sleep to meet.</p>
 
-
-
-
-
+    </div>
 
 
+    <h3>Here are some websites I've done in the past. Please click on the picture if you are interested</h3>
+
+    <Site
+      projectname="Learn for kids"
+      thumbnail={senti}
+      info="A site for small children to learn both how to use a computer and things like numbers while playing games!"
+      sitelink="https://sofieohlsson.github.io/learnforkids/"
+    />
+
+    <Site
+      projectname="Mario game"
+      thumbnail={mariogame}
+      info="I also made a mashup game of the classic pokémon fighting system with nintendo characters. ((**sound on**))"
+      sitelink="https://compassionate-lovelace-d33b19.netlify.com/"
+    />
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
   </Layout>
 )
